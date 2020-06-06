6f499fb3d035d6af34dfaff4034433ca0ce75c33
es1595 <es1595@bp1-login01.data.bp.acrc.priv>
2020-06-06 16:03:58 +0100

    PBS script is basically running, but not using GPU yet

--- scripts/PBSPro/run_bert_cqa.sh
+++ scripts/PBSPro/run_bert_cqa.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-#PBS -N bertcqa // job name
-#PBS -o pbs_output.log // output file
-#PBS -e pbs_err.log // error file
+# Job name
+#PBS -N bertcqa 
+
+# Output file
+#PBS -o pbs_output.log
+
+# Error file
+#PBS -e pbs_err.log
 
 # request resources and set limits
-#PBS -l walltime==24:00:00
-#PBS -l select=1:ncpus=24:ngpus=2:mem=32GB:ompthreads=24  // 'select' chooses number of nodes.
+##PBS -l walltime==24:00:00
+#PBS -l select=1:ncpus=8:ngpus=1:mem=16GB
+#:ompthreads=24
+# 'select' chooses number of nodes.
 
 #  load required modules
 module load lang/python/anaconda/pytorch lang/cuda
@@ -18,6 +25,6 @@ cd /work/es1595/text_ranking_bayesian_optimisation
 python -u BERT_cQA.py
 
 # To submit: qsub run_bert_cqa.sh
-# To display the queue: qstat -Q
+# To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
 # Display server status: qstat -B <server>
 # Display job information: qstat <jobID>
