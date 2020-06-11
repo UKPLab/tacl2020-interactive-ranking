c3fbb1ba526ac5303d7e5268fbfd91400114d24a
Edwin Simpson <edwin.simpson@gmail.com>
2020-06-11 17:14:41 +0100

    debugging
    Former-commit-id: 95d8b7086a08379b22b9f623ddbcc3d5401b3bdc

--- gppl/gp_classifier_vb.py
+++ gppl/gp_classifier_vb.py
@@ -1186,6 +1186,8 @@ class GPClassifierVB(object):
         elif out_feats is None and K_star is None and K_starstar is None:
             # use the training feature vectors
             self.K_star, self.K_starstar = self._get_training_cov()
+            logging('in predict f:')
+            logging(self.K_start.shape)
             if not full_cov:
                 self.K_starstar = np.diag(self.K_starstar)
 
