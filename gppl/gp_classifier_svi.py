38652832aca587977c6ab7fb3bff59c789f53426
Edwin Simpson <edwin.simpson@gmail.com>
2020-06-11 17:28:28 +0100

    logging typos

--- gppl/gp_classifier_svi.py
+++ gppl/gp_classifier_svi.py
@@ -451,14 +451,13 @@ class GPClassifierSVI(GPClassifierVB):
         if not self.use_svi:
             return super(GPClassifierSVI, self)._expec_f_output(Ks_starstar, Ks_star, mu0, full_cov, reuse_output_kernel)
 
-        logging('sorting out covpair_out:')
+        logging.debug('sorting out covpair_out:')
         logging.debug(self.covpair_out is None)
 
         if self.covpair_out is None or not reuse_output_kernel:
             self.covpair_out = scipy.linalg.solve(self.K_mm/self.s, Ks_star.T).T
             logging.debug('created a new covpair_out using Ks_star with shape:')
             logging.debug(Ks_star.shape)
-
 
         logging.debug(self.covpair_out.shape)
 
