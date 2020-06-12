734c47640e0c1c081524c83d11c723b6fd97ffd3
Edwin Simpson <edwin.simpson@gmail.com>
2020-06-12 18:34:29 +0100

    output folders for 10 interaction jobs

--- scripts/PBSPro/run_inter_sum_supert_2004_10.sh
+++ scripts/PBSPro/run_inter_sum_supert_2004_10.sh
@@ -26,11 +26,11 @@ cd /work/es1595/text_ranking_bayesian_optimisation
 python -u stage1_active_pref_learning.py H 0 duc04_supert_H "[random]" . 24 DUC2004 0 supert
 
 #  run the script for each DUC dataset with GPPL-IMP, GPPL-UNPA, GPPL-EIG, GPPL-Random, BT-Random.
-python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_imp_gpplhh "[imp]" . 24 DUC2004 10 supert
-python -u stage1_active_pref_learning.py LR     0 duc04_supert_ran_lr     "[random]" . 24 DUC2004 10 supert
-python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_ran_gpplhh "[random]" . 24 DUC2004 10 supert
-python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_unp_gpplhh "[pair_unc]" . 24 DUC2004 10 supert
-python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_eig_gpplhh "[eig]" . 24 DUC2004 10 supert
+python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_imp_gpplhh_10 "[imp]" . 24 DUC2004 10 supert
+python -u stage1_active_pref_learning.py LR     0 duc04_supert_ran_lr_10     "[random]" . 24 DUC2004 10 supert
+python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_ran_gpplhh_10 "[random]" . 24 DUC2004 10 supert
+python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_unp_gpplhh_10 "[pair_unc]" . 24 DUC2004 10 supert
+python -u stage1_active_pref_learning.py GPPLHH 0 duc04_supert_eig_gpplhh_10 "[eig]" . 24 DUC2004 10 supert
 
 # To submit: qsub run_bert_cqa.sh
 # To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
