2fa8b26eb59e17ace0d4d94a482caf584e1167f9
Edwin Simpson <edwin.simpson@gmail.com>
2020-06-15 18:43:23 +0100

    debug output

--- summariser/oracle/lno_ref_values.py
+++ summariser/oracle/lno_ref_values.py
@@ -3,12 +3,14 @@ import random
 
 from summariser.utils.misc import sigmoid
 
+
 class SimulatedUser:
     def __init__(self,ref_values,m=2.5):
         self.ref_values = ref_values
         self.temperature = m
 
-    def getPref(self,idx1,idx2):
+    def getPref(self, idx1, idx2):
+        print('idx1 = %i, idx2 = %i, available reference vals =%i' % (idx1, idx2, len(self.ref_values)))
         prob = sigmoid(self.ref_values[idx1]-self.ref_values[idx2], self.temperature)
 
         if random.random() <= prob:
