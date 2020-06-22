81195f7739522debe7c2ddba6354ddec1ce86a6a
Edwin Simpson <edwin.simpson@gmail.com>
2020-06-22 14:24:44 +0100

    separate the GPPLHHs results

--- scripts/PBSPro/run_inter_sum_supert_debug_s.sh
+++ scripts/PBSPro/run_inter_sum_supert_debug_s.sh
@@ -23,8 +23,8 @@ module load lang/python/anaconda/pytorch
 cd /work/es1595/text_ranking_bayesian_optimisation
 
 #  run the script for each DUC dataset with GPPL-IMP, GPPL-UNPA, GPPL-EIG, GPPL-Random, BT-Random.
-python -u stage1_active_pref_learning.py GPPLHHs 0 duc02_supert_imp_gpplhh_10 "[imp]" . 24 DUC2002 10 supert 200 2 results_lstest5
-python -u stage1_active_pref_learning.py GPPLHHs 0 duc02_supert_imp_gpplhh "[imp]" . 24 DUC2002 100 supert 200 2 results_lstest5
+python -u stage1_active_pref_learning.py GPPLHHs 0 duc02_supert_imp_gpplhhs_10 "[imp]" . 24 DUC2002 10 supert 200 2 results_lstest5
+python -u stage1_active_pref_learning.py GPPLHHs 0 duc02_supert_imp_gpplhhs "[imp]" . 24 DUC2002 100 supert 200 2 results_lstest5
 
 # To submit: qsub run_bert_cqa.sh
 # To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
