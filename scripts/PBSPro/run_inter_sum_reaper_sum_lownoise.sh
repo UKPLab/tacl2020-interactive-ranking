fc3d2690e1f8c94d09450f52c747e7f77503966c
Edwin Simpson <edwin.simpson@gmail.com>
2020-07-06 21:36:54 +0200

    reaper heuristics test

---
+++ scripts/PBSPro/run_inter_sum_reaper_sum_lownoise.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Run the interactive summarisation simulations with SUPERT
+
+# Job name
+#PBS -N intsum_rea_02
+
+# Output file
+#PBS -o pbs_intersumrea2001_10_output.log
+
+# Error file
+#PBS -e pbs_intersumrea2001_10_err.log
+
+# request resources and set limits
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=24:mem=128GB:ompthreads=24
+# 'select' chooses number of nodes.
+
+#  load required modules
+module load lang/python/anaconda/pytorch
+
+# We might need to add the global paths to our code to the pythonpath. Also set the data directories globally.
+cd /work/es1595/text_ranking_bayesian_optimisation
+
+# Run the script using heuristics only and no interactions.
+python -u stage1_active_pref_learning.py GPPL 0 duc01_reaper_gppl_20 "[imp,random,pair_unc,eig,tig]" . 4 DUC2001 20 april 200 1 results_reaper
+python -u stage1_active_pref_learning.py LR     0 duc01_reaper_lr_20     "[random,unc]" . 4 DUC2001 20 april 200 1 results_reaper
+
+python -u stage1_active_pref_learning.py GPPL 0 duc02_reaper_gppl_20 "[imp,random,pair_unc,eig,tig]" . 4 DUC2002 20 april 200 1 results_reaper
+python -u stage1_active_pref_learning.py LR     0 duc02_reaper_lr_20     "[random,unc]" . 4 DUC2002 20 april 200 1 results_reaper
+
+python -u stage1_active_pref_learning.py GPPL 0 duc04_reaper_gppl_20 "[imp,random,pair_unc,eig,tig]" . 4 DUC2004 20 april 200 1 results_reaper
+python -u stage1_active_pref_learning.py LR     0 duc04_reaper_lr_20     "[random,unc]" . 4 DUC2004 20 april 200 1 results_reaper
+
+
+# To submit: qsub run_bert_cqa.sh
+# To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
+# Display server status: qstat -B <server>
+# Display job information: qstat <jobID>
+
+# To monitor job progress:
+# qstat -f | grep exec_host
+# Find the node where this job is running.
+# ssh to the node.
+# tail /var/spool/pbs/spool/<job ID>.bp1.OU
