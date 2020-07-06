4a88fef776edc2b56cf4c71ee0c4d3dccd211b27
Edwin Simpson <edwin.simpson@gmail.com>
2020-07-06 13:41:26 +0200

    redo with correct oracle

--- scripts/PBSPro/run_inter_sum_supert_2001.sh
+++ scripts/PBSPro/run_inter_sum_supert_2001.sh
@@ -26,10 +26,11 @@ cd /work/es1595/text_ranking_bayesian_optimisation
 #python -u stage1_active_pref_learning.py H 0 duc01_supert_H "[random]" . 4 DUC2001 0 supert
 
 #  run the script for each DUC dataset with GPPL-IMP, GPPL-UNPA, GPPL-EIG, GPPL-Random, BT-Random.
-python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_imp_gpplhh "[imp]" . 12 DUC2001 100 supert 200 0.5 results_ls05 1
-python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_eig_gpplhh "[eig]" . 12 DUC2001 100 supert 200 0.5 results_ls05 1
-python -u stage1_active_pref_learning.py LR     0 duc01_supert_ran_lr     "[random]" . 4 DUC2001 100 supert 200 0.5 results_ls05 1
-python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_ran_gpplhh "[random]" . 12 DUC2001 100 supert 200 0.5 results_ls05 1
+python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_imp_gpplhh "[imp]" . 12 DUC2001 100 supert 200 1 results_1 1
+python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_eig_gpplhh "[eig]" . 12 DUC2001 100 supert 200 1 results_1 1
+python -u stage1_active_pref_learning.py LR     0 duc01_supert_ran_lr     "[random]" . 4 DUC2001 100 supert 200 1 results_1 1
+python -u stage1_active_pref_learning.py LR     0 duc01_supert_unc_lr     "[unc]" . 4 DUC2001 100 supert 200 1 results_1 1
+python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_ran_gpplhh "[random]" . 12 DUC2001 100 supert 200 1 results_1 1
 
 #python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_bi_imp_gpplhh "[imp]" . 12 DUC2001 100 supertreaper
 #python -u stage1_active_pref_learning.py GPPLHH 0 duc01_supert_bi_eig_gpplhh "[eig]" . 12 DUC2001 100 supertreaper 200 1 results 1
