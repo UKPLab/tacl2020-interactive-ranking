3c44de10296de025aea2b13658efd544528bff82
Edwin Simpson <edwin.simpson@gmail.com>
2020-07-06 18:22:52 +0200

    plot

--- stage1_plot_progress.py
+++ stage1_plot_progress.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 
-matplotlib.rcParams.update({'font.size': 10})
+matplotlib.rcParams.update({'font.size': 12})
 
 tasks = ['supert_duc2001', 'supert_bi_duc2001']  # 'bertcqa'  # 'coala' # 'duc2001' #
 styles = ['-', ':', '-.', '--']
