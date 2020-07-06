c06fb87c075c9f2dc5d623bc179ab220585c0687
Edwin Simpson <edwin.simpson@gmail.com>
2020-07-06 22:17:15 +0200

    fix tests with sum vs prior

--- scripts/PBSPro/run_inter_sum_reaper_sum_lownoise04.sh
+++ scripts/PBSPro/run_inter_sum_reaper_sum_lownoise04.sh
@@ -23,7 +23,7 @@ module load lang/python/anaconda/pytorch
 cd /work/es1595/text_ranking_bayesian_optimisation
 
 # Run the script using heuristics only and no interactions.
-python -u stage1_active_pref_learning.py GPPL 0 duc01_reaper_gppl_20 "[imp,random,pair_unc]" . 4 DUC2001 20 april 200 1 results_reaper_1 1
+python -u stage1_active_pref_learning.py GPPL 0 duc04_reaper_gppl_20 "[imp,random,pair_unc]" . 4 DUC2004 20 april 200 1 results_reaper_1 1
 
 # To submit: qsub run_bert_cqa.sh
 # To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
