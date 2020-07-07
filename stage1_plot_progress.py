941b5cb7528bfd45967652723268a4ae5a2572e2
Edwin Simpson <edwin.simpson@gmail.com>
2020-07-07 23:09:56 +0200

    err in plotting

--- stage1_plot_progress.py
+++ stage1_plot_progress.py
@@ -182,7 +182,8 @@ for t, task in enumerate(tasks):
                         result_data = result_data[metric]
 
                     print(result_file)
-
+                    print(method)
+                    print(result_data.index)
                     val = result_data[result_data.index == method]
 
                 my_results.append(val)
